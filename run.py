b71d35935c677b63f6f57c08d446ce87dd69acdd
Cam McKay <cam.mckay9@gmail.com>
2024-04-13 11:24:34 -0500

    implemented check for api key environment variable

--- run.py
+++ run.py
@@ -15,7 +15,11 @@ def main():
     parser.add_argument("--ai-out", "-O", dest="ai_out", default="Data/summaries", type=str, help="Directory to save summaries of scraped articles in.", required=False)
     args = parser.parse_args()
 
-    gemini_api_key = os.environ["GEMINI_API_KEY"]
+    if "GEMINI_API_KEY" in os.environ:
+        gemini_api_key = os.environ["GEMINI_API_KEY"]
+        summarizer = GeminiSummarizer(gemini_api_key)
+    else:
+        summarizer = None
 
     input_file = FileIO(args.file_in)
     urls = input_file.get_line_list()
@@ -23,15 +27,15 @@ def main():
         scraper = CNNScraper(url) #This uses the CNN scraper. You can implement the Scraper interface with another class and change just this line for another news site
         title = scraper.get_title()
         article = scraper.get_article()
-        summarizer = GeminiSummarizer(gemini_api_key)
-        summary = summarizer.summarize(article)
         filename = args.file_out + "/" + title.replace(" ", "_") + ".txt"
         summary_filename = args.ai_out + "/" + title.replace(" ", "_") + ".txt"
         if filename != args.file_out + "/.txt": # skips saving if article title ends up blank
             output_file = FileIO(filename)
             output_file.write_data(article)
-            summary_output_file = FileIO(summary_filename)
-            summary_output_file.write_data(summary)
+            if summarizer is not None:
+                summary = summarizer.summarize(article)
+                summary_output_file = FileIO(summary_filename)
+                summary_output_file.write_data(summary)
 
 if __name__=="__main__":
     main()
